211beaf0ef12f9d942a76bcf0205fdefd36fbee4
Andrew <argerrard@live.com>
2020-09-17 17:46:49 -0400

    Added initial check of user's entered verification code to activate account

--- services/users/verifyUserConfirmation.js
+++ services/users/verifyUserConfirmation.js
@@ -1,9 +1,132 @@
 var AWS = require("aws-sdk");
 var db = new AWS.DynamoDB.DocumentClient();
+var stepfunctions = new AWS.StepFunctions();
 
 exports.handler = async (event, context) => {
-  const { email, confirmationCode, verificationToken } = event;
+  const { email, confirmationCode } = event;
+
+  if (!email || !confirmationCode) {
+    return {
+      statusCode: 400,
+      body: {
+        error: 'Bad request - an email and confirmation code are required.'
+      }
+    };
+  }
+
   // Get the confirmationCode stored in the database for the user
+  // We can get the emailTaskToken at the same time to prevent unnecessary database reads
+  let { code, error, statusCode, emailTaskToken } = await getUserConfirmationCode(email);
+
+  if (error) {
+    return {
+      statusCode,
+      body: {
+        error
+      }
+    };
+  }
+
+  // By this point, a valid code was found
   // Verify that the supplied code matches the generated code
-  // Trigger the step function to continue and activate the user
+  if (!isEnteredCodeValid(confirmationCode, code)) {
+    return {
+      statusCode: 400,
+      body: {
+        error: "Sorry - that doesn't seem to be the right verification code."
+      }
+    };
+  }
+
+  console.info(`Verification successful for user ${email} - activating user.`);
+
+  // Activate the user
+  activateUser(email);
+
+  // Trigger step function to send confirmation e-mail and close off the workflow
 };
+
+
+/**
+ * Gets the correct registration confirmation code from the database as well as
+ * the task token required for calling back the registration e-mail step function.
+ * 
+ * @param {String} email - the email of the user we are getting the confirmation for 
+ */
+async function getUserConfirmationCode(email) {
+  const error = 'There was a problem validating your confirmation code.';
+  const params = {
+    "Key": { email },
+    "ProjectionExpression": "confirmationCode, isVerified, verificationToken",
+    "TableName": "Users"
+  };
+
+  try {
+    const result = await db.get(params).promise();
+
+    // Confirm if the result contains a user or if user does not exist
+    const user = result.Item;
+    if (!user) {
+      console.info(`Could not find user ${email} to confirm`);
+      return {
+        error,
+        statusCode: 400
+      };
+    }
+
+    // Check if the user is marked as verified in the system already
+    // If so, we don't need to go any further through the process
+    if (user.isVerified) {
+      console.info(`${email} tried to confirm but was already verified.`);
+      return {
+        error,
+        statusCode: 400
+      };
+    }
+
+    return {
+      code: user.confirmationCode,
+      emailTaskToken: user.verificationToken
+    };
+  } catch(err) {
+    console.error("Could not get user confirmation code.", err);
+    return {
+      error,
+      statusCode: 500
+    };
+  }
+}
+
+
+/**
+ * Helper function to confirm that the code supplied by the user is valid.
+ * Returns true if the verification code matches the database, false otherwise.
+ * 
+ * @param {Number} enteredCode - the code entered by the user
+ * @param {Number} correctCode - the code created when the user initially registered
+ */
+function isEnteredCodeValid(enteredCode, correctCode) {
+  return correctCode === enteredCode;
+}
+
+
+/**
+ * Function to activate the user in the database. This function is called with
+ * the e-mail of the user once the verification code has been validated.
+ * 
+ * @param {String} email - the email of the user to activate
+ */
+function activateUser(email) {
+  return null;
+}
+
+
+/**
+ * Function to continue the registration workflow by sending a confirmation to the user 
+ * that their account is successfully registered.
+ * 
+ * @param {String} emailTaskToken - the task token required to continue the registration workflow
+ */
+function sendConfirmationEmail(emailTaskToken) {
+  stepfunctions.sendTaskSuccess({ verificationToken: emailTaskToken }).promise();
+}
