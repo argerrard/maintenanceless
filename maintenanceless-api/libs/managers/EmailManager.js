0632bf03275f3953a328056b42bc3d7b878f924b
Andrew <argerrard@live.com>
2021-01-20 09:19:04 -0500

    fixed lint errors

--- maintenanceless-api/libs/managers/EmailManager.js
+++ maintenanceless-api/libs/managers/EmailManager.js
@@ -6,9 +6,9 @@ class EmailManager {
   /**
    * Sends an e-mail to the specified receipents using the subject and body
    * passed into the method. The sender of the email is specified by the source.
-   * 
+   *
    * The service used to send the e-mail is specified in the constructor of the class.
-   * 
+   *
    * @param {[String]} receipients - Array of receipts to send the e-mails to
    * @param {String} source - The sender of the e-mail
    * @param {String} subject - The subject of the email
